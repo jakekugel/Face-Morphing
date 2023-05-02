6b3f001f453818f74da12c505a60fcce7b340bd5
Jake Kugel <jake_kugel@yahoo.com>
2023-05-02 07:34:05 -0500

    Merging in image_dir and b_spline argument support

--- face_morphing_tool/app/do_morphing.py
+++ face_morphing_tool/app/do_morphing.py
@@ -1,63 +1,88 @@
 from __future__ import annotations
 
+import os
 import sys
+import traceback
 import cv2
+import numpy as np
 from pathlib import Path
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, Namespace, BooleanOptionalAction
 from face_morphing_tool.utils.face_landmark_detection import generate_face_correspondences
 from face_morphing_tool.utils.delaunay_triangulation import make_delaunay
 from face_morphing_tool.utils.face_morph import generate_morph_sequence
 
 
-def face_morphing(image1, image2, output, hide_lines=True, duration=5, frame_rate=20):
-    
+def face_morphing(image1, image2, image_dir, output, hide_lines, b_spline, duration=5, frame_rate=20):
+
 	"""
 	Entry point for the process
 
 	Arguments:
-		img1: path to the first image
-		img2: path to the second image
+		image1: path to the first image
+		image2: path to the second image
+		image_dir: path to directory containing images
 		output: output video path
 		hide_lines: hide the triangulation lines or not
+		b_spline: whether to use b-spline interpolation instead of standard linear interpolation
 		duration: duration of the generated video in seconds
 		frame_rate: the frame rate in fps
 
 	Returns:
 		int -- POSIX complaint exit code (0 for success)
-	
+
 	"""
-	
+
 	try:
-		image1 = cv2.imread(image1)
-		image2 = cv2.imread(image2)
+		if (image1 or image2) and image_dir:
+			raise Exception('the --image_dir argument cannot be used with the --image1 and --image2 arguments.')
 
-		[size, img1, img2, points1, points2, list3] = generate_face_correspondences(image1, image2)
+		# Read in images
+		if image1:
+			# Just two images given
+			image_test = cv2.imread(image1)
+			images = np.zeros((2, image_test.shape[0], image_test.shape[1], image_test.shape[2]), dtype=np.uint8)
+			images[0] = cv2.imread(image1)
+			images[1] = cv2.imread(image2)
+		else:
+			# Directory containing images given
+			pics = [os.path.join(image_dir, file) for file in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, file)) and file.lower().endswith('png')]
+			images = np.zeros((len(pics), 1024, 1024, 3), dtype=np.uint8)
+			for i in range(len(pics)):
+				images[i] = cv2.imread(pics[i])
 
-		tri = make_delaunay(size[1], size[0], list3, img1, img2)
+		if len(images) < 3 and b_spline:
+			raise Exception('the --b_spline parameter requires at least 3 images.')
 
-		generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, tri, size, output, hide_lines)
+		[image_shape, corr_points, transition_average_points] = generate_face_correspondences(images)
+
+		triangulations = make_delaunay(image_shape[1], image_shape[0], transition_average_points, images)
+
+		generate_morph_sequence(duration, frame_rate, images, corr_points, triangulations, image_shape, output, hide_lines, b_spline)
+
 		return 0
 	except Exception as e:
-	    return 1
+		print('Error: ' + str(e))
+		traceback.print_exception(e)
+		return 1
 
 
 def validate_args(args: Namespace):
 
 	"""
 	Validate the parsed argument
-	
+
 	Returns:
 		bool -- true if valid, false if invalid
 	"""
-	
+
 	valid = True
 
 	if not Path(args.imag1).exists():
-    	
-		valid = False    
-	
+
+		valid = False
+
 	if not Path(args.image2).exists():
-    	
+
 		valid = False
 
 	return valid
@@ -67,19 +92,24 @@ def main():
 	parser = ArgumentParser(usage="face-morphing "
 		"--image1 <path to the first image> "
 		"--image2 <path to the second image> "
-		"--output <path to the output video>"
-		"--hide_lines <hide the trangulation lines or not>"
+		"--output <path to the output video> "
+		"--hide_lines <hide the trangulation lines or not> "
 		"--duration <duration of the generated video in seconds> "
-		"--frame_rate <fame rate of the generated video in fps>",
+		"--frame_rate <fame rate of the generated video in fps> "
+		"--image_dir <directory containing images for face morph> "
+		"--b_spline <use b-spline interpolation>"
+		,
 		description="Do face-morphing based on two face images",
 	)
 
-	parser.add_argument("--image1", type=str, help="Path to the first image", required=True)
-	parser.add_argument("--image2", type=str, help="Path to the second image", required=True)
+	parser.add_argument("--image1", type=str, help="Path to the first image")
+	parser.add_argument("--image2", type=str, help="Path to the second image")
 	parser.add_argument("--output", type=str, help="Path to the output video", required=True)
-	parser.add_argument("--hide_lines",type=bool, default=True, help="Hide the triangulation lines")
 	parser.add_argument("--duration", type=int, default=5, help="duration of the generated video in seconds")
 	parser.add_argument("--frame_rate", type=int, default=20, help="frame rate of the generated video in fps")
+	parser.add_argument("--hide_lines", action=BooleanOptionalAction, help="Hide the triangulation lines" )
+	parser.add_argument("--image_dir", required=False, help="Directory containing images.")
+	parser.add_argument("--b_spline", action=BooleanOptionalAction, help="If provided, a b-spline is used to interpolate the position of the correspondence points.")
 
 	args = parser.parse_args()
 	print(args)
--- face_morphing_tool/utils/face_morph.py
+++ face_morphing_tool/utils/face_morph.py
@@ -6,6 +6,7 @@ import math
 from subprocess import Popen, PIPE
 from PIL import Image
 from face_morphing_tool.utils import misc
+import scipy.interpolate as interpolate
 
 # Apply affine transform calculated using srcTri and dstTri to src and
 # output an image of size.
@@ -67,53 +68,100 @@ def morph_triangle(img1, img2, img, t1, t2, t, alpha) :
         print('    r[3] = {}'.format(r[3]))
 
 
-def generate_morph_sequence(duration,frame_rate,img1,img2,points1,points2,tri_list,size,output, hide_lines):
-    
-    num_images = int(duration*frame_rate)
+def get_b_splines(points):
+    b_splines = []
+
+    for corr_point_num in range(len(points[0])):
+        x_vals = np.array([points[i][corr_point_num][0] for i in range(len(points))], dtype=np.float32)
+        y_vals = np.array([points[i][corr_point_num][1] for i in range(len(points))], dtype=np.float32)
+        t_vals = np.array([float(i) for i in range(len(points))], dtype=np.float32)
+
+        tx, cx, kx = interpolate.splrep(t_vals, x_vals, s=0, k=4)
+        b_spline_x = interpolate.BSpline(tx, cx, kx, extrapolate=False)
+
+        ty, cy, ky = interpolate.splrep(t_vals, y_vals, s=0, k=4)
+        b_spline_y = interpolate.BSpline(ty, cy, ky, extrapolate=False)
+
+        b_splines.append([b_spline_x, b_spline_y])
+
+    return b_splines
+
+
+def interpolate_points(points, b_splines, transition_num, j, num_frames):
+    interpolated_points = []
+
+    for corr_point_num in range(0, len(points[0])):
+        t = float(transition_num) + j / num_frames
+
+        spline_x = b_splines[corr_point_num][0]
+        spline_y = b_splines[corr_point_num][1]
+
+        x = spline_x(t)
+        y = spline_y(t)
+
+        interpolated_points.append( (x, y) )
+
+    return interpolated_points
+
+
+def generate_morph_sequence(duration, frame_rate, images, points, triangulations, size, output, hide_lines, b_spline):
+
+    # Number of frames per transition
+    num_frames = int(duration*frame_rate)
+
+    if b_spline:
+        b_splines = get_b_splines(points)
+
     p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-r', str(frame_rate),'-s',str(size[1])+'x'+str(size[0]), '-i', '-', '-c:v', 'libx264', '-crf', '25','-vf','scale=trunc(iw/2)*2:trunc(ih/2)*2','-pix_fmt','yuv420p', output], stdin=PIPE)
-    
-    for j in range(0, num_images):
 
-        # Convert Mat to float data type
-        img1 = np.float32(img1)
-        img2 = np.float32(img2)
+    for transition_num in range(len(triangulations)):
+        for j in range(0, num_frames):
 
-        # Read array of corresponding points
-        points = []
-        alpha = j/(num_images-1)
+            # Convert Mat to float data type
+            img1 = np.float32(images[transition_num])
+            img2 = np.float32(images[transition_num + 1])
 
-        # Compute weighted average point coordinates
-        for i in range(0, len(points1)):
-            x = (1 - alpha) * points1[i][0] + alpha * points2[i][0]
-            y = (1 - alpha) * points1[i][1] + alpha * points2[i][1]
-            points.append((x,y))
-        
-        # Allocate space for final output
-        morphed_frame = np.zeros(img1.shape, dtype = img1.dtype)
+            alpha = j/(num_frames - 1)
 
-        for i in range(len(tri_list)):    
-            x = int(tri_list[i][0])
-            y = int(tri_list[i][1])
-            z = int(tri_list[i][2])
-            
-            t1 = [points1[x], points1[y], points1[z]]
-            t2 = [points2[x], points2[y], points2[z]]
-            t = [points[x], points[y], points[z]]
+            # Interpolate correspondence points.
+            if b_spline:
+                # Use a b-spline to interpolate positions of correspondence points.
+                interpolated_points = interpolate_points(points, b_splines, transition_num, j, num_frames)
+            else:
+                # Use standard linear interpolation for positions of correspondence points.
+                interpolated_points = []
+                for i in range(0, len(points[transition_num])):
+                    x = (1 - alpha) * points[transition_num][i][0] + alpha * points[transition_num + 1][i][0]
+                    y = (1 - alpha) * points[transition_num][i][1] + alpha * points[transition_num + 1][i][1]
+                    interpolated_points.append((x,y))
 
-            # Morph one triangle at a time.
-            morph_triangle(img1, img2, morphed_frame, t1, t2, t, alpha)
-            
-            pt1 = (int(t[0][0]), int(t[0][1]))
-            pt2 = (int(t[1][0]), int(t[1][1]))
-            pt3 = (int(t[2][0]), int(t[2][1]))
 
-            if not hide_lines:
-                cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
-                cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
-                cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
-            
-        res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
-        res.save(p.stdin,'JPEG')
+            # Allocate space for final output
+            morphed_frame = np.zeros(img1.shape, dtype = img1.dtype)
+
+            for i in range(len(triangulations[transition_num])):
+                x = int(triangulations[transition_num][i][0])
+                y = int(triangulations[transition_num][i][1])
+                z = int(triangulations[transition_num][i][2])
+
+                t1 = [points[transition_num][x], points[transition_num][y], points[transition_num][z]]
+                t2 = [points[transition_num + 1][x], points[transition_num + 1][y], points[transition_num + 1][z]]
+                t = [interpolated_points[x], interpolated_points[y], interpolated_points[z]]
+
+                # Morph one triangle at a time.
+                morph_triangle(img1, img2, morphed_frame, t1, t2, t, alpha)
+
+                pt1 = (int(t[0][0]), int(t[0][1]))
+                pt2 = (int(t[1][0]), int(t[1][1]))
+                pt3 = (int(t[2][0]), int(t[2][1]))
+
+                if not hide_lines:
+                    cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
+                    cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
+                    cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
+
+            res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
+            res.save(p.stdin,'JPEG')
 
     p.stdin.close()
     p.wait()
